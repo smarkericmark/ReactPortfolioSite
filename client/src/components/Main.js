d6f2887c61bf705507d00d3921375b5674fa5c11
smarkericmark <markericberg@gmail.com>
2022-04-04 12:15:09 -0700

    first commit

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -6,10 +6,51 @@ export default function Main() {
         <div className="Main">
             {/*<h3 className="main--title">About</h3>*/}
             <p className="main--p"> I am an IT and Security Professional and a React/Flask developer passionate about security and automation. I built this site and set up and configured the cloud server it is currently hosting on.</p> 
-            <p className="main--p">In my previous life, I worked in corporate finance. I started as a part-time bank teller. Within four years was traveling across the US to meet and manage the relationships of corporate finance clients.</p>
+            
+            {/*<p className="main--p">In my previous life, I worked in corporate finance. I started as a part-time bank teller. Within four years was traveling across the US to meet and manage the relationships of corporate finance clients.</p>*/}
 
-            {/* <h3 className="main--title">Projects</h3>
-            */}
+            <h3 className="main--title">Projects</h3>
+            <div className="main--listBox">
+                <div className="main--list1">
+                    <ul>
+                        <li>
+                            <a href="https://www.mberg.ca/FightingGame01" target="_blank" className="main--ProjectButton">
+                                Fighting Game
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.mberg.ca/airbnb" target="_blank" className="main--ProjectButton">
+                                Airbnb
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.mberg.ca/bookreview" target="_blank" className="main--ProjectButton">
+                                Book Review</a>
+                        </li>
+                    </ul>
+                </div>
+            
+            
+            <div className="main--list2">
+                    <ul>
+                        <li>
+                            <a href="https://www.mberg.ca/memegenerator" target="_blank" className="main--ProjectButton">
+                                MemeGenerator
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.mberg.ca/classic" target="_blank" className="main--ProjectButton">
+                                My First Site
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.mberg.ca/teacozy" target="_blank" className="main--ProjectButton">
+                                Tea Cozy
+                            </a>
+                        </li>
+                    </ul>
+            </div>
+            </div>
         </div>
         
     )
--- flasksitetest/TestSite.py
+++ flasksitetest/TestSite.py
@@ -1,21 +1,43 @@
 from flask import Flask, render_template, send_from_directory, Blueprint
 app=Flask(__name__, static_folder="../client/build", static_url_path='/')
-some_blueprint = Blueprint('some_blueprint',__name__, static_folder="../client/build/Projects/airbnbclone/build", static_url_path='/',url_prefix='/')
+#some_blueprint = Blueprint('some_blueprint',__name__, static_folder="../client/build/Projects/FightingProject01", static_url_path='/',url_prefix='/')
 
 @app.route('/')
 def index():
 	return app.send_static_file('index.html')
 
-@app.route('/home')
+@app.route('/FightingGame01')
 def home():
-	return app.send_static_file('FolderTest/layouttest.html')
+	return app.send_static_file('Projects/FightingProject01/index.html')
 
-@some_blueprint.route('/test')
-def test():
-	return some_blueprint.send_static_file('index.html')
+@app.route('/airbnbclone')
+def airbnb():
+	return app.send_static_file('Projects/airbnbclone/build/index.html')
+
+@app.route('/teacozy')
+def teacozy():
+	return app.send_static_file('Projects/TeaCozy/index.html')
+
+@app.route('/classic')
+def classic():
+	return app.send_static_file('Projects/classicSite/layout.html')
+
+@app.route('/bookreview')
+def bookreview():
+	return app.send_static_file('Projects/bookreview/build/index.html')
+
+@app.route('/memegenerator')
+def memegenerator():
+	return app.send_static_file('Projects/memegenerator/build/index.html')
 
 
-app.register_blueprint(some_blueprint)
+
+#@some_blueprint.route('/test')
+#def test():
+#	return some_blueprint.send_static_file('index.html')
+
+
+#app.register_blueprint(some_blueprint)
 
 #@app.route('/test')
 #def test():
